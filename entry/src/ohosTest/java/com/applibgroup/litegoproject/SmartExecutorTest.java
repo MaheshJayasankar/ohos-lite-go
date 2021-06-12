440461999e455869ec4e1bfac978fe30ec25307f
elaitenstile <47279543+elaitenstile@users.noreply.github.com>
2021-06-12 17:19:25 +0530

    Added Debug mode Test

--- entry/src/ohosTest/java/com/applibgroup/litegoproject/SmartExecutorTest.java
+++ entry/src/ohosTest/java/com/applibgroup/litegoproject/SmartExecutorTest.java
@@ -6,7 +6,6 @@ import com.applibgroup.harmony_os_lite_go.SmartExecutor;
 import com.applibgroup.harmony_os_lite_go.utils.GoUtil;
 import org.junit.Test;
 
-
 public class SmartExecutorTest {
     @Test
     public void testGoUtil(){
@@ -25,5 +24,20 @@ public class SmartExecutorTest {
         assert(smartExecutor2.getCoreSize() > 0);
         SmartExecutor smartExecutor3 = new SmartExecutor(2,2, SchedulePolicy.FirstInFirstRun, OverloadPolicy.DiscardNewTaskInQueue);
         assert(smartExecutor3.getCoreSize() > 0);
+    }
+    @Test
+    public void testSmartExecutorDebugMode()
+    {
+        SmartExecutor smartExecutor = new SmartExecutor();
+        smartExecutor.setDebug(true);
+
+        smartExecutor.execute(() -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        smartExecutor.awaitAll();
     }
 }
